a83f088affd8b0479099475c7aa5377efcb66596
RoloSeeMo <102483420+RoloSeeMo@users.noreply.github.com>
2025-06-12 01:05:32 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const OpenMenuButton = document.querySelector("#menu-open-button");
-const CloseMenuButton = document.querySelector("#menu-close-button");
-
-
-OpenMenuButton.addEventListener("click", ()=> {
-    document.body.classList.toggle("show-mobile-menu");
-});
-
-CloseMenuButton.addEventListener("click", ()=> OpenMenuButton.click());
