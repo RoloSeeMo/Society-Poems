bbcfbff4b22aae8be9304b250ab631a9fb19ecb3
RoloSeeMo <102483420+RoloSeeMo@users.noreply.github.com>
2025-06-27 16:32:20 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
-// This would be in a file like /functions/index.js
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 // This function triggers whenever a user is deleted from Authentication
 exports.cleanupUser = functions.auth.user().onDelete((user) => {
   const uid = user.uid;
   console.log(`User ${uid} deleted, cleaning up database profile.`);
-  
+
   // Return the promise from the remove() operation
   return admin.database().ref(`/users/${uid}`).remove();
 });
